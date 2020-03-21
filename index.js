a5bfda378d070b8304a3e60ec7df27afa3136a1a
Nicolas Borboën <ponsfrilus@gmail.com>
2020-03-21 13:50:57 +0100

    [WIP] Maaany things
    * Player model now understand more data from steam API, as personaname, 
    realname, avatar....
    * Player model handle telegram id, nick, names
    * Adding a player now validate SteamID against the steam API
    * index.js now use await/async with async-request
    * Some bot commands are now aliased in english
    * `/start` and `/help` commands added
    * Janitorized: no more semicolon, no more tabs
    * Still a lot to do....

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const request = require('request');
+//const request = require('request')
+const request = require('async-request')
 const Telegraf = require('telegraf')
-const Data = require('./data');
-const FS = require('fs');
-const Config = require('./config');
+const Data = require('./data')
+const FS = require('fs')
+const Config = require('./config')
 
 // ------------------------------------------------------------------- constants
 const SteamBUrl="https://api.steampowered.com/ISteamUser/"
@@ -13,102 +14,150 @@ const helpMsg = `Command reference:
 /stacca STEAMID - remove a given steamId from the list
 /lista - List all configured users
 /boh - Show this help page
-`;
+`
 
 // ------------------------------------------------------------------- functions
 
+function start(ctx) {
+  ctx.reply(helpMsg)
+}
 function logMsg(ctx) {
-    var from = userString(ctx);
-    console.log('<', ctx.message.text, from)
+  var from = userString(ctx)
+  console.log('<', ctx.message.text, from)
 }
 
 function logOutMsg(ctx, text) {
-    console.log('>', {
-        id: ctx.chat.id
-    }, text);
+  console.log('>', {
+    id: ctx.chat.id
+  }, text)
 }
 
 function userString(ctx) {
-    return JSON.stringify(ctx.from.id == ctx.chat.id ? ctx.from : {
-        from: ctx.from,
-        chat: ctx.chat
-    });
+  return JSON.stringify(ctx.from.id == ctx.chat.id ? ctx.from : {
+    from: ctx.from,
+    chat: ctx.chat
+  })
 }
 
 // wrap a request in an promise
-function downloadPage(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) reject(error);
-            if (response.statusCode != 200) {
-                reject('Invalid status code <' + response.statusCode + '>');
-            }
-            resolve(body);
-        });
-    });
+function OLDdownloadPage(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error)
+      if (response.statusCode != 200) {
+        reject('Invalid status code <' + response.statusCode + '>')
+      }
+      resolve(body)
+    })
+  })
+}
+
+async function downloadPage(url) {
+  try {
+    response = await request(url);
+    console.log('RESPONSE...', response.body)
+    if (response.statusCode != 200) {
+      console.error('Invalid status code <' + response.statusCode + '>')
+    } else {
+      return response.body
+    }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 function getPlayers(ctx) {
-	Data.getPlayersSteamIds(ctx).then( player_ids => {
-		var url = SteamBUrl+"GetPlayerSummaries/v2/?key=" 
-		        + Config.steamKey + "&steamids=" + player_ids.join(",")
-		downloadPage(url).then( json => {
-			const players = JSON.parse(json).response.players
-			var out = ""
-			for (const player of players) {
-				if (player.gameid !== undefined) {
-					g = player.gameextrainfo === undefined ? player.gameid : player.gameextrainfo
-					p = player.realname + " (" + player.personaname +"): " + g
-					out = out + p + "\n"
-				}
-			}
-			ctx.reply(out)					
-		})
-	})
+  Data.getPlayersSteamIds(ctx).then( player_ids => {
+  var url = SteamBUrl+"GetPlayerSummaries/v2/?key="
+          + Config.steamKey + "&steamids=" + player_ids.join(",")
+  downloadPage(url).then( json => {
+    const players = JSON.parse(json).response.players
+    console.log(players)
+    var out = ""
+    for (const player of players) {
+      if (player.gameid !== undefined) {
+          g = player.gameextrainfo === undefined ? player.gameid : player.gameextrainfo
+          p = player.realname + " (" + player.personaname +"): " + g
+          out = out + p + "\n"
+      }
+    }
+    ctx.reply(out)
+    })
+  })
+}
+
+async function validateSteamID(id) {
+  let url = SteamBUrl+"GetPlayerSummaries/v2/?key="
+  + Config.steamKey + "&steamids=" + id
+  // console.debug('url', url)
+  let json = await downloadPage(url)
+  const player = JSON.parse(json).response.players[0]
+  // console.debug(player)
+  if (player && 'steamid' in player) {
+    return player
+  } else {
+    return false
+  }
 }
 
 // TODO: at most 100 players allowed otherwise steam request have to be splitted
-function addPlayer(ctx) {
- 	const m = ctx.message.text.match(/\/tacca +([0-9]+)( +(.*))?$/)
- 	if (m === null) {
- 		ctx.reply("Syntax Error")
- 	} else {
-		Data.addPlayer(ctx, {steam: m[1], nick: m[3]}).then( player => {
-			ctx.reply("Ok. Player " + player.steam + " added")
-		})
- 	}
+async function addPlayer(ctx) {
+  const m = ctx.message.text.match(/^\/tacca|\/add +([0-9]+)( +(.*))?$/)
+  if (m === null) {
+    ctx.reply("Syntax Error")
+  } else {
+    player = await validateSteamID(m[1])
+    if (player) {
+      player.nick = m[3]
+      player.tg_id = ctx.message.from.id
+      player.tg_nick = ctx.message.from.username || null
+      player.tg_first_name = ctx.message.from.first_name || null
+      player.tg_last_name = ctx.message.from.last_name || null
+      Data.addPlayer(ctx, player).then( player => {
+        ctx.replyWithMarkdown('Ok. Player [' + player.personaname + '](http://steamcommunity.com/profiles/' + player.steamid + ') #' + player.steamid + ' added')
+      })
+    } else {
+      console.error('Sorry, player', m[3], 'not found...')
+      ctx.reply('Sorry, player' + m[3] + 'not found...')
+    }
+  }
 }
 
 function delPlayer(ctx) {
- 	const m = ctx.message.text.match(/\/stacca +([0-9]+)$/)
- 	if (m === null) {
- 		ctx.reply("Syntax Error")
- 	} else {
- 		const steam = m[1]
-		Data.delPlayerBySteam(ctx, steam).then( (res) => {
-			ctx.reply("Ok. Player " + steam + " removed")
-		})
- 	}
+  const m = ctx.message.text.match(/^\/stacca|\/rm +([0-9]+)$/)
+  if (m === null) {
+    ctx.reply("Syntax Error")
+  } else {
+    const steamid = m[1]
+    Data.delPlayerBySteam(ctx, steamid).then( (res) => {
+      ctx.reply("Ok. Player " + steamid + " removed")
+    })
+  }
 }
 
 
 function listPlayers(ctx) {
-	Data.getPlayers(ctx).then( players => {
-		var out = ""
-		for (const player of players) {
-			out = out + player.steam + " - " + player.nick + "\n"
-		}
-		ctx.reply(out)
-	})
+  Data.getPlayers(ctx).then( players => {
+    var out = ""
+    for (const player of players) {
+      out = out + player.steamid + " - " + player.nick + "\n"
+    }
+    ctx.reply(out)
+  })
 }
 
 // ------------------------------------------------------------------------- bot
-// dataService.loadSteams(Config);
+// dataService.loadSteams(Config)
 const bot = new Telegraf(Config.botToken)
 bot.command('chi', getPlayers)
-// bot.command('add', )
-bot.command('boh', ctx => {ctx.reply(helpMsg)});
+bot.command('get', getPlayers)
+bot.command('help', ctx => {ctx.reply(helpMsg)})
+bot.command('boh', ctx => {ctx.reply(helpMsg)})
+bot.command('add', addPlayer)
 bot.command('tacca', addPlayer)
+bot.command('rm', delPlayer)
 bot.command('stacca', delPlayer)
 bot.command('lista', listPlayers)
+bot.command('list', listPlayers)
+bot.command('start', start)
 bot.launch()
